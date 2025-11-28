957a6dd3bf0e6c08b92ccdd8c6032ec7358b523e
rahultech09 <rahulk.tech09@gmail.com>
2025-11-28 12:52:50 +0530

    app maturity amount added in dashboard

--- src/app/pages/dashboad/dashboad.page.ts
+++ src/app/pages/dashboad/dashboad.page.ts
@@ -18,7 +18,7 @@ export class DashboadPage implements OnInit {
   ac_type:string = ''
   clientInfo:any = ''
   pendig_data:any;
-  version:string = '32';
+  version:string = '33';
   // account service info
 
 
